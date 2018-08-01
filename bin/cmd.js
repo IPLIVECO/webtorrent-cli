4743f677aeff3edf60691e45c3df1b3512174e27
IPLIVECO <iplice.co@gmail.com>
2018-08-01 01:06:19 -0700

    cmd

--- bin/cmd.js
+++ bin/cmd.js
@@ -589,6 +589,8 @@ var drawInterval
 function drawTorrent (torrent) {
   if (!argv.quiet) {
     process.stdout.write(Buffer.from('G1tIG1sySg==', 'base64')) // clear for drawing
+    setTimeout(draw, 1000)
+    setTimeout(draw, 10000)
     setTimeout(draw, 30000)
   }
 
